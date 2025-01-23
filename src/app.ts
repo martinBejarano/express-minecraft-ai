0337220eea56f9b92b8577a8d5678225d16522cd
martinBejarano <martinezequielbejaranoo@gmail.com>
2025-01-23 17:56:31 -0300

    fix Overall fix

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import './config/config.ts';
 
 import express from 'express';
-import passport from 'passport';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
-import './config/passport.ts';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { aiRoutes } from './routes/index.js';
@@ -23,17 +21,14 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use('/api/ai', aiRoutes);
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB', err);
-});
+// mongoose.connect(process.env.MONGODB_URI).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch((err) => {
+//   console.error('Error connecting to MongoDB', err);
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
